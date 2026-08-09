4a4b04c32aeb49aa3d7daed7162254fa2e6ad0c3
Pedro <pedrooareas06@gmail.com>
2026-08-08 23:46:03 -0300

    Fix:Correção

--- pokerole-v2/frontend/app/(dashboard)/layout.tsx
+++ pokerole-v2/frontend/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
-import { AppSidebar } from "@/components/layout/app-sidebar";
-import { AppHeader } from "@/components/layout/app-header";
+import { AppSidebar } from "@/components/layout/appSidebar";
+import { AppHeader } from "@/components/layout/appHeader";
 
 export default function DashboardLayout({
   children,
